abdc03ed6d900a5c2ab713036bd3eb2180f33a25
Logan Bell <loganbell@gmail.com>
2011-06-15 21:50:08 -0700

    Renamed tetris.js to faketris.js

---
+++ faketris.js
@@ -0,0 +1,431 @@
+ /*------------------------------------------------------------------------*\
+ *  FakeTris 0.1
+ *  Written by: Logan J. Bell <loganbell@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *-------------------------------------------------------------------------*/
+
+
+(function(window){
+    // Game variables
+
+    var TABLEHEIGHT = 20;
+    var TABLEWIDTH  = 10;
+    var PAUSE       = 0;
+    var GAMESPEED   = 500;
+    var score       = 0;
+    var VERSION     = 0.3;
+    var Game_Assets = window['Game_Assets'];     
+    var gametimer;
+
+    var statuswindow;
+    var scorewindow;
+    var current_shape;
+    var current_color;
+    var current_imgbg;
+
+    // Key Being Constants
+    var P       = 80;    
+    var LEFT    = 37;
+    var UP      = 38;
+    var DOWN    = 40;
+    var RIGHT   = 39;
+
+    var Shape = function(){
+
+			var self = this;
+            self.currentposition = 0; 
+            self.currentshape = 1;
+            self.positions = new Array;
+
+            self.init = function(){
+                self.currentshape = Math.floor(Math.random() * self.shapes.length);
+                self.positions = self.shapes[self.currentshape].positions;
+                self.currentposition = Math.floor(Math.random() * self.shapes[self.currentshape].positions.length);
+            };
+
+            self.get_current_position = function(){
+                return self.shapes[self.currentshape].positions[self.currentposition];
+            };
+
+            self.get_current_color = function(){
+                return self.shapes[self.currentshape].color;
+            };
+
+            self.get_current_bg = function(){
+                return "url(data:image/gif;base64," + Game_Assets.Images[self.shapes[self.currentshape].color];
+            };
+
+            self.get_current_shape = function(){
+                return self.currentshape;
+            };
+
+            self.movedown = function() {
+
+        		var current_position = self.get_current_position();
+
+        		for (y in current_position){
+        		    var coord = current_position[y];
+        		    if (document.getElementById(coord)){
+        		        document.getElementById(coord).style.backgroundImage=current_imgbg;
+        		        document.getElementById(coord).style.backgroundColor=current_color;
+        		    }
+        		} 
+
+                for (x in self.positions){
+                    for (y in self.positions[x]){
+                        self.positions[x][y][0]++;
+                    }
+                }
+
+            };
+
+    		self.stop_block = function(){ 
+    		    var current_position = self.get_current_position();
+    		    for (y in current_position){
+    		        var coord = current_position[y];
+    		        if (coord[0] === 0){
+			    	    clearInterval(gametimer);
+			    	    gametimer = null;
+    		            statuswindow.innerHTML = 'Game Over';
+    		            document.getElementById(coord).className = 'edge';
+    		            document.getElementById(coord).style.backgroundColor=current_color;
+    		            document.getElementById(coord).style.backgroundImage=current_imgbg;
+    		            break;
+    		        }
+    		        document.getElementById(coord).className = 'edge';
+    		        document.getElementById(coord).style.backgroundColor=current_color;
+    		        document.getElementById(coord).style.backgroundImage=current_imgbg;
+    		    }
+    		};
+    		/*
+    		    Description: scans table for any complete rows
+    		    Input: None
+    		    Output: int
+    		*/
+    		self.look_down = function(){
+    		    var edge = 0;
+				if ( self != null) {
+    		    	var current_position = self.get_current_position();
+    		    	for (y in current_position){
+    		    	        var coord = current_position[y];
+    		    	        var nextcoord = [current_position[y][0]+1,current_position[y][1]];
+    		    	        if (document.getElementById(nextcoord) && document.getElementById(nextcoord).className === 'edge'){
+    		    	            edge = 1;
+    		    	        } else if (coord[0] == (TABLEHEIGHT-1) ){
+    		    	            edge = 1;
+    		    	            document.getElementById(coord).className = 'edge';
+    		    	        }
+    		    	}
+				}
+    		    return edge;
+    		};
+
+
+            self.moveright = function(){
+				var moveright_ok = 1;
+				var current_position = self.get_current_position();
+               	for (y in current_position){
+               	    var coord = current_position[y];
+               	    var nextcoord = [current_position[y][0],current_position[y][1]+1];
+               	    if (current_position[y][1]+1 == TABLEWIDTH || document.getElementById(nextcoord).className === 'edge'){
+               	        moveright_ok = 0;
+               	    }
+               	    if (!document.getElementById(nextcoord)){
+               	        moveright_ok = 0;
+               	    }
+               	}
+               	if (moveright_ok > 0){
+               		for (x in self.positions){
+               	     for (y in self.positions[x]){
+               	         self.positions[x][y][1]++;
+               	     }
+               	 	}
+				}
+            }; 
+
+            self.moveleft = function(){
+            	var moveleft_ok = 1;
+            	var current_position = self.get_current_position();
+            	for (y in current_position){
+            	    var coord = current_position[y];
+            	    var nextcoord = [current_position[y][0],current_position[y][1]-1];
+            	    if (current_position[y][1]- 1 == 0 && document.getElementById(nextcoord).className === 'edge'){
+            	        moveleft_ok = 0;
+            	    }
+            	    if (!document.getElementById(nextcoord)){
+            	        moveleft_ok = 0;
+            	    }
+            	}
+            	if (moveleft_ok > 0){
+                	for (x in self.positions){
+                	    for (y in self.positions[x]){
+                	        self.positions[x][y][1]--;
+                	    }
+                	}
+				}
+            }; 
+
+            self.rotate = function() {
+                self.currentposition = self.currentposition >= (self.positions.length - 1) ? 0 : self.currentposition + 1;
+            }; 
+
+            self.shapes = [
+                {
+                    'positions':[[[0,3],[0,4],[0,5],[0,6]],[[0,4],[-1,4],[-2,4],[-3,4]]],
+                    'color': 'blue' }, 
+                {
+                    'positions':[[[0,5],[0,6],[1,5],[1,6]]],
+                    'color': 'green'
+                }, 
+                {
+                    'positions':[[[0,3],[0,4],[-1,4],[-1,5]],[[-1,4],[0,4],[0,5],[1,5]]],
+                    'color': 'yellow'
+                }, 
+                {
+                    'positions':[[[0,3],[0,4],[0,5],[-1,4]],[[-1,4],[0,4],[1,4],[0,5]],[[0,3],[0,4],[0,5],[1,4]],[[-1,4],[0,4],[1,4],[0,3]]],
+                    'color': 'red'
+                } 
+            ];
+
+    };
+
+	var Grid = function (game_div) {
+		var self = this;
+	
+        self.game_element 		= document.getElementById(game_div);
+        self.statuswindow 		= document.createElement("div");
+        self.statuswindow.id 	= 'status_window';
+        self.statuswindow.innerHTML 	= 'Game Started';
+
+    	/*
+    	    Description: builds game gride
+    	    Input: None
+    	    Output: None
+    	*/
+		self.build_grid = function(){
+
+        	self.game_element.appendChild(self.statuswindow);
+
+        	var scorewindow 		= document.createElement("div");
+        	scorewindow.id 			= 'score_window';
+        	scorewindow.innerHTML 	= 'Score is: ' + score;
+        	self.game_element.appendChild(scorewindow);
+
+	    	var table = document.createElement("Table");
+        	table.className="grid";
+	    	for(i = 0; i < TABLEHEIGHT; i++){
+	    		var row = table.insertRow(i);
+	    		for(j=0; j < TABLEWIDTH; j++){
+	    			var cell = row.insertCell(j);
+	    			cell.style.backgroundColor = 'black';
+        	        cell.id = i+','+j;
+	    		}	
+	    	}	
+	    	self.game_element.appendChild(table);
+
+		}
+
+	};
+
+    /*
+        Description: Binds keys for game play
+        Input: None
+        Output: None
+    */
+    bind_keys = function(){
+        document.onkeydown = function(e){
+			var keynum;
+			if(window.event){
+				keynum = e.keyCode;
+			}
+			else{
+				keynum = e.which;
+			}
+
+			if (current_shape) {
+				switch (keynum) {
+					case UP:
+            	    	current_shape.rotate();    
+						break;
+					case DOWN:
+        				var reset_table = scan_for_complete_rows()
+        				if (current_shape.look_down()){
+        				    current_shape.stop_block();
+        				}else{
+        				    current_shape.movedown();
+        				}
+						break;
+					case RIGHT:	
+				    	current_shape.moveright();
+						break;
+					case LEFT:
+						current_shape.moveleft();
+						break;
+            		case PAUSE:
+            	    	PAUSE = PAUSE === 1 ? 0 : 1;
+            	    	if(PAUSE === 1){
+						    clearInterval(gametimer);
+							gametimer = null;
+            	    	    statuswindow.innerHTML = 'Game Paused';
+            	    	}else{
+            	    	    gametimer = setInterval(gameplay,GAMESPEED);
+            	    	    statuswindow.innerHTML = 'Game Started';
+            	    	}
+						break;
+							
+				}
+			}
+		}
+
+    };
+
+    /*
+        Description: pauses game via clearing gametime
+        Input: None
+        Output: None
+    */
+    pause_game = function(){
+		    clearInterval(gametimer);
+		    gametimer = null;
+
+    };
+   
+    /*
+        Description: pauses game via clearing gametime
+        Input: None
+        Output: None
+    */
+    start_game = function(){ 
+            gametimer = setInterval(gameplay,GAMESPEED);
+    };
+
+    /*
+        Description: scans table for any complete rows
+        Input: None
+        Output: int
+    */
+    scan_for_complete_rows = function()
+    {
+        var reset_table = 0;
+
+	    for(i = 0; i < TABLEHEIGHT; i++){
+            var cellcount = 0;
+	    	for(j=0; j < TABLEWIDTH; j++){
+	    		var cell = document.getElementById(i+','+j);
+                if (cell.className != 'edge'){
+	    		    cell.style.backgroundColor = 'black';
+	    		    cell.style.backgroundImage = '';
+                }
+                else{
+                    cellcount++;    
+                }
+	    	}	
+            if(cellcount == TABLEWIDTH){
+                reset_table = i;
+            }
+	    }
+
+        return reset_table;
+
+    };
+
+    /*
+        Description: scans table for any complete rows
+        Input: None
+        Output: int
+    */
+    clear_table = function(reset_table){
+	    for(x = reset_table; x > 0; x--){
+	        for(j=0; j < TABLEWIDTH; j++){
+                var row = x;
+                var col = j;
+                
+                if (row <= reset_table){
+                    prevrow = row - 1;
+                    var prev_elem = document.getElementById(prevrow+','+col);
+                    if (prev_elem){
+                        var prev_bg_color = document.getElementById(prevrow+','+col).style.backgroundColor; 
+                        var prev_bg_class = document.getElementById(prevrow+','+col).className;
+                        document.getElementById(row+','+col).style.backgroundColor = prev_bg_color;
+                        document.getElementById(row+','+col).className = prev_bg_class;
+                        document.getElementById(prevrow+','+col).style.className = '';
+                    }
+                }
+            }
+        }       
+        GAMESPEED *= .90;
+    };
+
+
+
+    /*
+        Description: Primary interface function in which the game is played
+        Input: None
+        Output: None
+    */
+    var gameplay = function() {
+        if (!current_shape){
+            current_shape = new Shape;
+            current_shape.init();
+            current_color = current_shape.get_current_color();
+            current_imgbg = current_shape.get_current_bg();
+        }
+
+        var reset_table = scan_for_complete_rows()
+        if (reset_table){
+            pause_game();
+            update_score();
+            clear_table(reset_table);
+            start_game();
+        }
+
+        if (current_shape.look_down()){
+            current_shape.stop_block();
+			stop_block();
+        }else{
+            current_shape.movedown();
+        }
+    };
+
+    /*
+        Description: stops a falling block if an edge is enountered
+                     and detects if a block has ended the game.
+        Input: None
+        Output: None
+    */
+    stop_block = function(){ 
+		current_shape = null;
+    };
+
+    update_score = function() {
+        scorewindow.innerHTML = 'Score is: ' + ++score;
+    };
+
+    /*
+        Description: Public method to begin game
+        Input: None
+        Output: None
+    */
+    window['Game'] = { 
+        start:function(game_div){
+			var grid = new Grid(game_div);	
+            grid.build_grid();   
+            bind_keys();
+		    gametimer = setInterval(gameplay,GAMESPEED);
+        }
+    };
+}(window));
+
